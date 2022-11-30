de0533ebb4fe824850639447b3d2b0042aa6cd23
Philip <philip.robinson@globalfishingwatch.org>
2022-11-30 10:29:32 -0800

    allow for multipolygon and minor refactor

--- antimeridian_splitter/geopolygon_utils.py
+++ antimeridian_splitter/geopolygon_utils.py
@@ -1,8 +1,15 @@
+from enum import Enum
 import json
 from typing import List, Union
 
 from shapely import affinity
 from shapely.geometry import GeometryCollection, Polygon, mapping
+
+
+class OutputFormat(Enum):
+    Geojson = 'geojson'
+    Polygons = 'polygons'
+    GeometryCollection = 'geometrycollection'
 
 
 def check_crossing(lon1: float, lon2: float, validate: bool = True, dlon_threshold: float = 180.0):
@@ -14,13 +21,29 @@ def check_crossing(lon1: float, lon2: float, validate: bool = True, dlon_thresho
         raise ValueError("longitudes must be in degrees [-180.0, 180.0]")   
     return abs(lon2 - lon1) > dlon_threshold
 
-def translate_polygons(geometry_collection: GeometryCollection, 
-                       output_format: str = "geojson") -> Union[List[dict], List[Polygon]]:
-    
-  for polygon in geometry_collection.geoms:
-      (minx, _, maxx, _) = polygon.bounds
-      if minx < -180: geo_polygon = affinity.translate(polygon, xoff = 360)
-      elif maxx > 180: geo_polygon = affinity.translate(polygon, xoff = -360)
-      else: geo_polygon = polygon
 
-      yield json.dumps(mapping(geo_polygon)) if (output_format == "geojson") else geo_polygon
+def translate_polygons(
+    geometry_collection: GeometryCollection,
+    output_format: OutputFormat = OutputFormat.Geojson
+) -> Union[List[dict], List[Polygon], GeometryCollection]:
+
+    geo_polygons = []
+    for polygon in geometry_collection.geoms:
+        (minx, _, maxx, _) = polygon.bounds
+        if minx < -180:
+            geo_polygon = affinity.translate(polygon, xoff = 360)
+        elif maxx > 180:
+            geo_polygon = affinity.translate(polygon, xoff = -360)
+        else:
+            geo_polygon = polygon
+
+        geo_polygons.append(geo_polygon)
+
+    if output_format == OutputFormat.Polygons:
+        result = geo_polygons
+    if output_format == OutputFormat.Geojson:
+        result = [json.dumps(mapping(p)) for p in geo_polygons]
+    elif output_format == OutputFormat.GeometryCollection:
+        result = GeometryCollection(geo_polygons)
+
+    return result
--- antimeridian_splitter/split_polygon_test.py
+++ antimeridian_splitter/split_polygon_test.py
@@ -1,5 +1,9 @@
 from .split_polygon import split_polygon
+from .geopolygon_utils import OutputFormat
 import json
+from functools import reduce
+from shapely.geometry.base import BaseGeometry
+import shapely.geometry
 
 example_polygon1 = """
 {
@@ -14,21 +18,58 @@ example_polygon1 = """
 
 def test_split_polygon_to_geometrycollection():
     poly: dict = json.loads(example_polygon1)
-    res = split_polygon(poly, 'geometrycollection')
+    res = split_polygon(poly, OutputFormat.GeometryCollection)
     assert str(res) == 'GEOMETRYCOLLECTION (POLYGON ((180 0, 179 0, 179 1, 180 1, 180 0)), POLYGON ((-180 1, -179 1, -179 0, -180 0, -180 1)))'
 
 def test_split_polygon_to_geojson():
     poly: dict = json.loads(example_polygon1)
-    res = split_polygon(poly, 'geojson')
-    assert len(res) == 2
+    res = split_polygon(poly, OutputFormat.Geojson)
     assert str(res) == """['{"type": "Polygon", "coordinates": [[[180.0, 0.0], [179.0, 0.0], [179.0, 1.0], [180.0, 1.0], [180.0, 0.0]]]}', '{"type": "Polygon", "coordinates": [[[-180.0, 1.0], [-179.0, 1.0], [-179.0, 0.0], [-180.0, 0.0], [-180.0, 1.0]]]}']"""
+    assert len(res) == 2
 
 def test_split_polygon_to_polygons():
     poly: dict = json.loads(example_polygon1)
-    res = split_polygon(poly, 'polygons')
+    res = split_polygon(poly, OutputFormat.GeometryCollection)
     assert len(res) == 2
     string_repr = str([str(poly) for poly in res])
     expect = """['POLYGON ((180 0, 179 0, 179 1, 180 1, 180 0))', 'POLYGON ((-180 1, -179 1, -179 0, -180 0, -180 1))']"""
     assert expect == string_repr
 
+
+example_multipolygon = {
+    'type': 'MultiPolygon',
+    'coordinates': [
+        [
+            [
+                [179.0, 55.0],
+                [179.0, 56.0],
+                [179.0, 56.0],
+                [179.0, 57.0],
+                [179.0, 57.0],
+                [180.0, 58.0],
+                [180.0, 55.0],
+                [179.0, 55.0]
+            ],
+        ],
+        [
+            [
+                [-180.0, 58.0],
+                [-179.0, 58.0],
+                [-175.0, 57.0],
+                [-175.0, 57.0],
+                [-175.0, 56.0],
+                [-176.0, 56.0],
+                [-176.0, 55.0],
+                [-176.0, 55.0],
+                [-180.0, 55.0],
+                [-180.0, 58.0]
+            ],
+        ]
+    ]
+}
+
+def test_split_multipolygons():
+    polygon_collection = split_polygon(example_multipolygon, OutputFormat.Polygons)
+    target_multipolygon = reduce(BaseGeometry.union, polygon_collection)
+    assert json.dumps(example_multipolygon) == json.dumps(shapely.geometry.mapping(target_multipolygon))
 
